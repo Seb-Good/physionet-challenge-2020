e4e83598d7e47fa82486ab98b2546a86fe1cdf22
rosie <sebi.goodfellow@gmail.com>
2020-04-24 19:15:17 -0400

    - initial commit.

--- run_12ECG_classifier.py
+++ run_12ECG_classifier.py
@@ -1,7 +1,15 @@
+import numpy as np
+
+
 def run_12ECG_classifier(data, header_data, classes, model):
 
-    current_label = [1, 0, 0, 0, 0, 0, 0, 0, 0]
-    current_score = [0.9, 0.6, 0.2, 0.05, 0.2, 0.35, 0.35, 0.1, 0.1]
+    # current_label = [1, 0, 0, 0, 0, 0, 0, 0, 0]
+    # current_score = [0.9, 0.6, 0.2, 0.05, 0.2, 0.35, 0.35, 0.1, 0.1]
+
+    num_classes = len(classes)
+    current_label = np.zeros(num_classes, dtype=int)
+    current_score = np.zeros(num_classes)
+    current_label[0] = 1
 
     return current_label, current_score
 
