5ff42d09fbff7da0c92f2e603f7c43f2a4f8097b
rosie <sebi.goodfellow@gmail.com>
2020-05-02 09:05:12 -0400

    - debugged deepecg

--- kardioml/models/deepecg/model/model.py
+++ kardioml/models/deepecg/model/model.py
@@ -14,6 +14,7 @@ import tensorflow as tf
 
 # Local imports
 from kardioml.models.deepecg.model.graph import Graph
+from kardioml.models.deepecg.networks.deepecg_v1 import DeepECGV1
 
 
 class Model(object):
