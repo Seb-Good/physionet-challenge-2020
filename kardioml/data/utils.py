60597142636b902722eb1e6ddd75ee87ba5ce856
rosie <sebi.goodfellow@gmail.com>
2020-06-21 22:13:51 -0400

    - updated ETL code for new datasets.

--- kardioml/data/utils.py
+++ kardioml/data/utils.py
@@ -15,7 +15,7 @@ def get_snomedct_concept_by_id(id_number):
     base_url = 'https://browser.ihtsdotools.org/snowstorm/snomed-ct'
     edition = 'MAIN'
     version = '2019-07-31'
-    url = base_url + '/browser/' + edition + '/' + version + '/concepts/' + id_number
+    url = base_url + '/browser/' + edition + '/' + version + '/concepts/' + str(id_number)
     response = urlopen(url).read()
     data = json.loads(response.decode('utf-8'))
     return data['fsn']['term']
