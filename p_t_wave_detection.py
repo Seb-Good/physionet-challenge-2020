d88c55bcf21ee623736fc3e77abc78ee89e3e3cf
Dmitrii Shubin <shubin.dmitrii.n@gmail.com>
2020-07-27 16:14:27 -0400

    added numba abstraction on peak detector

--- p_t_wave_detection.py
+++ p_t_wave_detection.py
@@ -27,8 +27,98 @@ class PTWaveDetection(nn.Module):
         self.padding_LPF = int((self.weights_LPF.shape[2] - 1) / 2)
         self.padding_LPF = torch.Tensor(np.zeros((self.padding_LPF))).cuda()
 
-    @numba.jit(nopython=True, parallel=True)
-    def peakdet(self, v, delta: float):
+
+
+    def FIR_filt(self, input, weight, padding_vector):
+        input = torch.cat((input, padding_vector), 0)
+        input = torch.cat((padding_vector, input), 0)
+        input = input.view(1, 1, input.shape[0])
+        output = torch.conv1d(input, weight, bias=None, stride=1, padding=0, dilation=1, groups=1)
+        output = output.view(output.shape[2])
+        return output
+
+    def forward(self, x):
+
+        x -= torch.mean(x)
+
+        # LPF for p and T waves cleaning
+        x = self.FIR_filt(x, self.weights_LPF, self.padding_LPF)
+
+        # standard scaling
+        x = (x - torch.mean(x)) / torch.std(x)
+
+        return x
+
+    def run(self, X, rpeaks):
+
+
+
+        self.eval()
+        X = torch.tensor(X, dtype=torch.float)
+        X = X.cuda()
+        X_filt = self.forward(X)
+
+        X_filt = X_filt.cpu().detach().numpy()
+        X = X.cpu().detach().numpy()
+
+
+
+        twaves = []
+        pwaves = []
+
+
+        for i in range(len(rpeaks)):
+
+            if i == 0:
+                PR = X_filt[0 : rpeaks[i]].copy()
+                RT = X_filt[rpeaks[i] : int(rpeaks[i] + 3 * (rpeaks[i + 1] - rpeaks[i]) / 4)].copy()
+            elif i == len(rpeaks) - 1:
+                PR = X_filt[int(rpeaks[i] - 1 * (rpeaks[i] - rpeaks[i - 1]) / 4) : rpeaks[i]].copy()
+                RT = X_filt[rpeaks[i] :].copy()
+            else:
+                PR = X_filt[int(rpeaks[i] - 1 * (rpeaks[i] - rpeaks[i - 1]) / 4) : rpeaks[i]].copy()
+                RT = X_filt[rpeaks[i] : int(rpeaks[i] + 3 * (rpeaks[i + 1] - rpeaks[i]) / 4)].copy()
+
+            PR /= max(PR)
+            RT /= max(RT)
+
+            T_wave, _ = peakdet(RT, 1 / 6)
+            P_wave, _ = peakdet(PR, 0.1 / 6)
+
+            if len(T_wave) > 1:
+                T_wave = T_wave[1]
+                twaves.append(rpeaks[i] + T_wave[0])
+
+            if len(P_wave) > 0:
+                # print(len(PR))
+                # print(P_wave[-1][0])
+                if len(PR) - P_wave[-1][0] < 45:
+                    del P_wave[-1]
+                if len(P_wave) > 0:
+                    P_wave = P_wave[-1]
+                    if i == 0:
+                        pwaves.append(P_wave[0])
+                    else:
+                        pwaves.append(int(rpeaks[i] - 1 * (rpeaks[i] - rpeaks[i - 1]) / 4) + P_wave[0])
+
+
+        for i in range(len(twaves)):
+            twaves[i] = [twaves[i], X[int(twaves[i])]]
+
+        twaves = np.array(twaves)
+
+        for i in range(len(pwaves)):
+            pwaves[i] = [pwaves[i], X[int(pwaves[i])]]
+
+
+
+        twaves = np.array(twaves)
+        pwaves = np.array(pwaves)
+        return twaves, pwaves, X_filt
+
+
+@numba.jit(nopython=False,parallel=False)
+def peakdet(v, delta: float):
 
         # list of peaks
         maxtab = []
@@ -67,96 +157,3 @@ class PTWaveDetection(nn.Module):
                     lookformax = True
 
         return maxtab, mintab
-
-    def FIR_filt(self, input, weight, padding_vector):
-        input = torch.cat((input, padding_vector), 0)
-        input = torch.cat((padding_vector, input), 0)
-        input = input.view(1, 1, input.shape[0])
-        output = torch.conv1d(input, weight, bias=None, stride=1, padding=0, dilation=1, groups=1)
-        output = output.view(output.shape[2])
-        return output
-
-    def forward(self, x):
-
-        x -= torch.mean(x)
-
-        # LPF for p and T waves cleaning
-        x = self.FIR_filt(x, self.weights_LPF, self.padding_LPF)
-
-        # standard scaling
-        x = (x - torch.mean(x)) / torch.std(x)
-
-        return x
-
-    def run(self, X, rpeaks):
-
-        start = time()
-
-        self.eval()
-        X = torch.tensor(X, dtype=torch.float)
-        X = X.cuda()
-        X_filt = self.forward(X)
-
-        X_filt = X_filt.cpu().detach().numpy()
-        X = X.cpu().detach().numpy()
-
-        stop = time()
-        print('Time for filtering: ',stop-start)
-
-        twaves = []
-        pwaves = []
-
-
-        for i in range(len(rpeaks)):
-
-            if i == 0:
-                PR = X_filt[0 : rpeaks[i]].copy()
-                RT = X_filt[rpeaks[i] : int(rpeaks[i] + 3 * (rpeaks[i + 1] - rpeaks[i]) / 4)].copy()
-            elif i == len(rpeaks) - 1:
-                PR = X_filt[int(rpeaks[i] - 1 * (rpeaks[i] - rpeaks[i - 1]) / 4) : rpeaks[i]].copy()
-                RT = X_filt[rpeaks[i] :].copy()
-            else:
-                PR = X_filt[int(rpeaks[i] - 1 * (rpeaks[i] - rpeaks[i - 1]) / 4) : rpeaks[i]].copy()
-                RT = X_filt[rpeaks[i] : int(rpeaks[i] + 3 * (rpeaks[i + 1] - rpeaks[i]) / 4)].copy()
-
-            PR /= max(PR)
-            RT /= max(RT)
-
-            start = time()
-            T_wave, _ = self.peakdet(RT, 1 / 6)
-            P_wave, _ = self.peakdet(PR, 0.1 / 6)
-            stop = time()
-            print('Time for peak detection: ', stop - start)
-
-            if len(T_wave) > 1:
-                T_wave = T_wave[1]
-                twaves.append(rpeaks[i] + T_wave[0])
-
-            if len(P_wave) > 0:
-                # print(len(PR))
-                # print(P_wave[-1][0])
-                if len(PR) - P_wave[-1][0] < 45:
-                    del P_wave[-1]
-                if len(P_wave) > 0:
-                    P_wave = P_wave[-1]
-                    if i == 0:
-                        pwaves.append(P_wave[0])
-                    else:
-                        pwaves.append(int(rpeaks[i] - 1 * (rpeaks[i] - rpeaks[i - 1]) / 4) + P_wave[0])
-
-
-        start = time()
-        for i in range(len(twaves)):
-            twaves[i] = [twaves[i], X[twaves[i]]]
-
-        twaves = np.array(twaves)
-
-        for i in range(len(pwaves)):
-            pwaves[i] = [pwaves[i], X[pwaves[i]]]
-
-        stop = time()
-        print('Time for final peak selection: ', stop - start)
-
-        twaves = np.array(twaves)
-        pwaves = np.array(pwaves)
-        return twaves, pwaves, X_filt
