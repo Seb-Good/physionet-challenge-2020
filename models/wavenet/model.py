5361c0542b7b78fa15f5aa3a470475a50cd3daaf
Dmitrii Shubin <shubin.dmitrii.n@gmail.com>
2020-08-11 16:25:55 -0400

    added forced usage of 8 gpus

--- models/wavenet/model.py
+++ models/wavenet/model.py
@@ -41,7 +41,7 @@ class Model:
 
         if torch.cuda.device_count() > 1:
             print("Number of GPUs will be used: ", torch.cuda.device_count())
-            self.model = DP(self.model)
+            self.model = DP(self.model, device_ids=list(range(torch.cuda.device_count())))
         else:
             print('Only one GPU is available')
 
