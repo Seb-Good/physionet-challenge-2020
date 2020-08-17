8d93d271ef01ea668dc5054bda8a9fdc88cab1a2
Dmitrii Shubin <shubin.dmitrii.n@gmail.com>
2020-08-17 13:31:29 -0400

    n epoch = 1 for tests in sh scripts

--- train_0.sh
+++ train_0.sh
@@ -1,1 +1,1 @@
-python main.py --start_fold 0 --n_epochs 100 --batch_size 128 --lr 0.001 --gpu 0,1 --downsample True
+python main.py --start_fold 0 --n_epochs 1 --batch_size 128 --lr 0.001 --gpu 0,1 --downsample True
