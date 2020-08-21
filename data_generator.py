45dac8704572e7830a38024fe88a6227cd685e41
Dmitrii Shubin <shubin.dmitrii.n@gmail.com>
2020-08-21 00:21:48 -0400

    added normalization for hr

--- data_generator.py
+++ data_generator.py
@@ -181,7 +181,7 @@ class Preprocessing():
         if y['hr'] is None:
             X = np.concatenate([X, np.zeros((X.shape[0], 1))], axis=1)
         else:
-            X = np.concatenate([X, np.zeros((X.shape[0], 1)) + y['hr']], axis=1)
+            X = np.concatenate([X, np.zeros((X.shape[0], 1)) + y['hr']/100], axis=1)
 
 
 
